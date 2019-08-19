6cc683ebb27ca13b28e8e4385e2e902c481088d5
DexterMrC <2758996752@qq.com>
2019-08-19 08:08:22 +0800

    第一次上传测试

---
+++ src/main/java/First/SubmitTest.java
@@ -0,0 +1,12 @@
+package First;
+
+/**
+ * @Author :  DexterMrC
+ * @Date :    2019/8/19 7:56
+ * @Description :
+ */
+public class SubmitTest {
+    public static void main(String[] args) {
+        System.out.println("第一次提交GitHub测试类");
+    }
+}
